0f8b97a45414c963697bedbc5aa8e997bfd51aba
ouhhyeaah <103272850+ouhhyeaah@users.noreply.github.com>
2023-11-26 16:00:14 -0500

    update

--- front-end-aws/src/App.js
+++ front-end-aws/src/App.js
@@ -23,6 +23,7 @@ const App = () => {
   };
 
   return (
+
     <Router>
       <Routes>
         <Route
--- front-end-aws/src/Connection.js
+++ front-end-aws/src/Connection.js
@@ -1,16 +1,16 @@
 import RegistrationForm from "./registerComponent";
 import LoginForm from "./loginComponent";
 import './Connection.css'
+import { Link } from "react-router-dom";
 
 
 const Connection = () => {
     return(
         <div className="container">
             <div className="card">
+                <h3> Inscription </h3>
                 < RegistrationForm />
-                <p className="result">
-
-                </p>
+                <Link to='/login'> Deja uin compte ?</Link>
             </div>    
         </div>
     )
